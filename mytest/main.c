a461e52c3db135db599f7832195818a153f6034b
Benjamin BOUMENDIL <benjamin.boumendil@student.42.fr>
2015-02-06 18:03:49 +0100

    pull

--- mytest/main.c
+++ mytest/main.c
@@ -6,7 +6,7 @@
 /*   By: bboumend <bboumend@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/05 17:31:49 by bboumend          #+#    #+#             */
-/*   Updated: 2015/02/05 23:48:46 by bboumend         ###   ########.fr       */
+/*   Updated: 2015/02/06 18:01:59 by bboumend         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ int			main(void)
 	ft_putstr("MY PRINTF : \n");
 	ft_printf("TEST\n");
 	ft_printf("%s\n%d\n%c\n", "test", 42, 48);
+	ft_printf("%s", NULL);
 
 	ft_putstr("\nREAL PRINTF : \n");
-	printf("TEST\n");
-	printf("%s\n%d\n%c\n", "test", 42, 48);
+	// printf("TEST\n");
+	// printf("%s\n%d\n%c\n", "test", 42, 48);
+	printf("%s", 0);
 
 	return (0);
 }
