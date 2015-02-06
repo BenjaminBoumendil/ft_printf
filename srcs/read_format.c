88eca5b6378b9cac2d6422dc8b92a25f14ee592b
Benjamin BOUMENDIL <benjamin.boumendil@student.42.fr>
2015-02-06 20:21:22 +0100

    norme

--- srcs/read_format.c
+++ srcs/read_format.c
@@ -3,26 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   read_format.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ochase <ochase@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bboumend <bboumend@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/03 18:08:46 by bboumend          #+#    #+#             */
-/*   Updated: 2015/02/06 18:54:37 by ochase           ###   ########.fr       */
+/*   Updated: 2015/02/06 20:21:11 by bboumend         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+#define F0 int		(*f[])(t_data *data) = {
+#define F1 parse_flags,
+#define F2 parse_width,
+#define F3 parse_precision,
+#define F4 parse_modifier,
+#define F5 parse_token};
+#define F F0 F1 F2 F3 F4 F5
+
 static void			init_parse(t_data *data)
 {
-	size_t			i;
-	int (*f[])(t_data *data) =
+	size_t	i;
 
-	{						\
-		parse_flags,		\
-		parse_width,		\
-		parse_precision,	\
-		parse_modifier,		\
-		parse_token};
+	F;
 	i = 0;
 	data->format++;
 	while (i < sizeof(f) / sizeof(*f))
