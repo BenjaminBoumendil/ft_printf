acd5bd4afa293dfd4281630a641254f9f41fde1d
Oscar CHASE <ochase@student.42.fr>
2015-02-07 23:27:27 +0100

    parser beton

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: ochase <ochase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/05 23:24:25 by ochase            #+#    #+#             */
-/*   Updated: 2015/02/07 23:12:24 by ochase           ###   ########.fr       */
+/*   Updated: 2015/02/07 23:25:42 by ochase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		parse_flags(t_data *data)
 	{ '0', ft_zero },  \
 	{ '-', ft_minus }, \
 	{ '+', ft_plus }};
-	while (data->format)
+	while (*data->format)
 	{
 		c = 0;
 		while (c < (sizeof(flags) / sizeof(t_flags)))
@@ -69,7 +69,7 @@ int		parse_modifier(t_data *data)
 	{ "l", ft_l },   \
 	{ "j", ft_j },   \
 	{ "z", ft_z }};
-	while (data->format)
+	while (*data->format)
 	{
 		c = 0;
 		while (c < (sizeof(mod) / sizeof(t_token)))
@@ -91,22 +91,27 @@ int		parse_modifier(t_data *data)
 int		parse_token(t_data *data)
 {
 	size_t			c;
-	const t_token	token[] =
+	const t_flags	token[] =
 
 	{              \
-	{ "s", ft_s }, \
-	{ "d", ft_d }, \
-	{ "c", ft_c }};
-	c = 0;
-	while (c < (sizeof(token) / sizeof(t_token)))
+	{ 's', ft_s }, \
+	{ 'd', ft_d }, \
+	{ 'c', ft_c }};
+	while (*data->format)
 	{
-		if (!ft_strncmp(token[c].str, data->format, ft_strlen(token[c].str)))
+		c = 0;
+		while (c < (sizeof(token) / sizeof(t_flags)))
 		{
-			data->format += ft_strlen(token[c].str);
-			token[c].f(data);
-			break ;
+			if (token[c].c == *data->format)
+			{
+				data->format++;
+				token[c].f(data);
+				break ;
+			}
+			c++;
 		}
-		c++;
+		if (c == (sizeof(token) / sizeof(t_flags)))
+			data->format++;
 	}
 	return (0);
 }
