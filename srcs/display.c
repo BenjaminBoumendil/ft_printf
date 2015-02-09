746b3d28489edd16575b3ac5ce50ff64bf26a4ec
Oscar CHASE <ochase@student.42.fr>
2015-02-09 13:46:22 +0100

    corrected mistakes

--- srcs/display.c
+++ srcs/display.c
@@ -6,13 +6,13 @@
 /*   By: ochase <ochase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/09 13:13:37 by ochase            #+#    #+#             */
-/*   Updated: 2015/02/09 13:41:37 by ochase           ###   ########.fr       */
+/*   Updated: 2015/02/09 13:45:55 by ochase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-static void	display_padding(char *str, char c, size_t len, size_t min_width)
+static void	display_padding(char c, size_t len, size_t min_width)
 {
 	size_t	i;
 
@@ -25,25 +25,23 @@ static void	display_padding(char *str, char c, size_t len, size_t min_width)
 }
 void		display(t_data *data, char *str)
 {
-	size_t	c;
 	size_t	len;
 
-	c = 0;
 	len = ft_strlen(str);
 	if (data->flag->minus)
 	{
 		ft_putstr(str);
 		if (data->min_width < len)
-			display_padding(str, ' ', len, data->min_width);
+			display_padding(' ', len, data->min_width);
 	}
 	else
 	{
 		if (data->min_width < len)
 		{
 			if (data->flag->zero)
-				display_padding(str, '0', len, data->min_width);
+				display_padding('0', len, data->min_width);
 			else
-				display_padding(str, ' ', len, data->min_width);
+				display_padding(' ', len, data->min_width);
 		}
 		ft_putstr(str);
 	}
